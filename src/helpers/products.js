686be2cb9ef82eca62906a51f5c556750b074073
robert-virtual <robertocastill63@gmail.com>
2021-12-01 22:33:22 -0600

    agrege atributo link a los elementos

--- src/helpers/products.js
+++ src/helpers/products.js
@@ -1,5 +1,6 @@
 const { default: axios } = require("axios");
 const cheerio = require("cheerio");
+const baseUrl = "https://www.gamestop.com";
 
 async function getProducts(url) {
   const resp = await axios.get(url);
@@ -7,13 +8,18 @@ async function getProducts(url) {
   const $ = cheerio.load(html);
   const items = [];
 
+  const links = $(".product-grid-tile-wrapper .product-tile-link", html);
+
   const imgs = $(".product-grid-tile-wrapper .tile-image", html);
   const prices = $(".product-grid-tile-wrapper .actual-price", html);
   $(".pd-name", html).each((i, el) => {
+    let link = $(links[i]).attr("href");
+    link = link.startsWith("http") ? link : baseUrl + link;
     items.push({
       name: $(el).text(),
       img: $(imgs[i]).attr("data-src"),
       price: $(prices[i]).text(),
+      link,
     });
   });
   return items;
